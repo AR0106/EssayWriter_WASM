cadf0de039eec2df964e7b85049d5171584f7b72
Alex Ritt <AlexRitt06@outlook.com>
2021-02-22 21:36:58 -0600

    Add project files.

---
+++ EssayWriter_ASP/Models/Writer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EssayWriter_ASP.Models
+{
+    public class Writer
+    {
+        [Required]
+        public string Thesis { get; set; }
+
+        public string Structures { get; set; }
+
+        [Required]
+        [Range(0.00, 1.00)]
+        public double temperature { get; set; }
+
+        [Required]
+        [Range(0.00, 1.00)]
+        public double pLevel { get; set; }
+
+        public string paragraphType { get; set; }
+    }
+}
